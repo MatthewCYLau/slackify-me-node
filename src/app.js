4e51603bbb2edf7b293506db30ce385e0073d087
Matthew Lau <matlau@deloitte.co.uk>
2019-11-09 16:16:33 +0000

    added unit tests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
-const port = process.env.PORT
 const path = require('path');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
-const auth = require('./middleware/auth')
-
 
 //Setup cookie parser
 app.use(cookieParser());
@@ -17,9 +14,6 @@ require('./db/mongoose')
 //Setup routers
 const messageRouter = require('./routers/message');
 const userRouter = require('./routers/user');
-
-//Setup data models
-const Message = require('./models/message');
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -43,37 +37,4 @@ app.use(express.json())
 app.use(messageRouter);
 app.use(userRouter);
 
-//Routing
-app.get('', (req, res) => {
-
-    const userLoggedIn = (req.cookies['auth_token'])
-
-    Message.find({}).limit(5).sort({
-        'time': 'desc'
-    }).exec(function (err, foundMessages) {
-
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("index", {
-                messages: foundMessages,
-                user: userLoggedIn
-            });
-        }
-
-    });
-});
-
-app.get('/success', auth, (req, res) => {
-    res.render('success', {
-        user: req.user
-    });
-})
-
-app.get('*', (req, res) => {
-    res.render('404');
-})
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+module.exports = app
